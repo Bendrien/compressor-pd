7dcd580a6ccf401d011fbc1b6e36f8ce58d7cd47
Anyere <anyere.bendrien@posteo.de>
2018-09-23 17:29:27 +0200

    Basic dsp

--- library.c
+++ library.c
@@ -1,23 +1,56 @@
 #include "library.h"
 
+t_int *compressor_tilde_perform(t_int *w)
+{
+    t_compressor_tilde *x = (t_compressor_tilde *)(w[1]);
+    t_sample          *in =           (t_sample *)(w[2]);
+    t_sample         *out =           (t_sample *)(w[3]);
+    int                 n =                  (int)(w[4]);
+
+    while (n--)
+    {
+        // *out++ = (*in++)*1.f;
+    }
+
+    return (w+5);
+}
+
+void compressor_tilde_dsp(t_compressor_tilde *x, t_signal **sp)
+{
+    dsp_add(compressor_tilde_perform, 4, x,
+            sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+}
+
 void *compressor_tilde_new(void)
 {
     t_compressor_tilde *x = (t_compressor_tilde *)pd_new(compressor_tilde_class);
 
+    x->out = outlet_new(&x->x_obj, &s_signal);
+
     return (void *)x;
 }
 
-void compressor_tilde_bang(t_compressor_tilde *x)
+void compressor_tilde_free(t_compressor_tilde *x)
 {
-    post("Hello world !!");
+    outlet_free(x->out);
 }
 
 void compressor_tilde_setup(void)
 {
     compressor_tilde_class = class_new(gensym("compressor~"),
                                        (t_newmethod) compressor_tilde_new,
-                                       0, sizeof(t_compressor_tilde),
-                                       CLASS_DEFAULT, 0);
+                                       (t_method) compressor_tilde_free,
+                                       sizeof(t_compressor_tilde),
+                                       CLASS_DEFAULT,
+                                       0);
 
-    class_addbang(compressor_tilde_class, compressor_tilde_bang);
+    class_addmethod(compressor_tilde_class,
+                    (t_method)compressor_tilde_dsp,
+                    gensym("dsp"),
+                    A_CANT,
+                    0);
+
+    CLASS_MAINSIGNALIN(compressor_tilde_class,
+                       t_compressor_tilde,
+                       in);
 }
--- library.h
+++ library.h
@@ -7,10 +7,16 @@ static t_class *compressor_tilde_class;
 
 typedef struct _compressor_tilde {
     t_object x_obj;
+
+    t_float in;
+    t_outlet *out;
 } t_compressor_tilde;
 
+t_int *compressor_tilde_perform(t_int *w);
+
+void compressor_tilde_dsp(t_compressor_tilde *x, t_signal **sp);
 void *compressor_tilde_new(void);
-void compressor_tilde_bang(t_compressor_tilde *x);
+void compressor_tilde_free(t_compressor_tilde *x);
 
 void compressor_tilde_setup(void);
 
